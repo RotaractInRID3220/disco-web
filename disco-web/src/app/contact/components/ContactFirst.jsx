8241607b2f2e1e847530683e4c03052e866a923f
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-06-30 19:53:33 +0530

    Update ContactFirst.jsx

--- disco-web/src/app/contact/components/ContactFirst.jsx
+++ disco-web/src/app/contact/components/ContactFirst.jsx
@@ -16,7 +16,7 @@ const ContactFirst = () => {
             </div>
             <div className='border border-cranberry rounded-lg px-8 py-10'>
                     <h1 className="font-bebas text-3xl text-gray-400 tracking-wide">CONTACT OUR CLUB SERVICE TEAM</h1>
-                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">DRR. Rtr. PP. Abheetha Aturupane</h1>
+                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">Rtr. PP. Abheetha Aturupane</h1>
                 <div className='lg:flex gap-5'>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry mt-5 hover:bg-cranberry hover:text-white transition-all"><FaPhoneAlt/>+94 70 3935 356</a>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />abheetha.rotaract3220@gmail.com</a>
@@ -24,7 +24,7 @@ const ContactFirst = () => {
             </div>
             <div className='border border-cranberry rounded-lg px-8 py-10'>
                     <h1 className="font-bebas text-3xl text-gray-400 tracking-wide uppercase">CONTACT OUR Organizational Development TEAM</h1>
-                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">DRR. Rtr. IPP Ahmadh Booso</h1>
+                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">Rtr. IPP Ahmadh Booso</h1>
                 <div className='lg:flex gap-5'>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry mt-5 hover:bg-cranberry hover:text-white transition-all"><FaPhoneAlt/>+94 77 3771 379</a>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />ahmadhbooso@gmail.com</a>
@@ -35,7 +35,7 @@ const ContactFirst = () => {
        <div className=' lg:w-1/2 mt-10 flex flex-col gap-10 lg:translate-y-10'>
             <div className='border border-cranberry rounded-lg px-8 py-10'>
                     <h1 className="font-bebas text-3xl text-gray-400 tracking-wide">CONTACT OUR Secretary</h1>
-                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">DRR. Rtr. PP Anagi  Gunasekera</h1>
+                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">Rtr. PP Anagi  Gunasekera</h1>
                 <div className='lg:flex gap-5'>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry mt-5 hover:bg-cranberry hover:text-white transition-all"><FaPhoneAlt/>+94 77 2307 141</a>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />anagi.rotaract3220@gmail.com</a>
@@ -43,7 +43,7 @@ const ContactFirst = () => {
             </div>
             <div className='border border-cranberry rounded-lg px-8 py-10'>
                     <h1 className="font-bebas text-3xl text-gray-400 tracking-wide uppercase">CONTACT OUR International Service TEAM</h1>
-                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">DRR. Rtr. IPP Ovini Wijethunga</h1>
+                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">Rtr. IPP Ovini Wijethunga</h1>
                 <div className='lg:flex gap-5'>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry mt-5 hover:bg-cranberry hover:text-white transition-all"><FaPhoneAlt/>+94 70 5830 221</a>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />oviniw.rotaract3220@gmail.com</a>
@@ -51,10 +51,10 @@ const ContactFirst = () => {
             </div>
             <div className='border border-cranberry rounded-lg px-8 py-10'>
                     <h1 className="font-bebas text-3xl text-gray-400 tracking-wide">CONTACT OUR DIGITAL SERVICES TEAM</h1>
-                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">DRR. Rtr. IPP Sandinu Pinnawala</h1>
+                    <h1 className="font-bebas text-5xl text-cranberry tracking-wide">Rtr. IPP Sandinu Pinnawala</h1>
                 <div className='lg:flex gap-5'>
                     <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry mt-5 hover:bg-cranberry hover:text-white transition-all"><FaPhoneAlt/>+94 77 951 8001</a>
-                    <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />sandinu.rotaract 3220@gmail.com</a>
+                    <a href="" className="flex items-center  gap-2 text-gray-600 rounded-lg py-2 px-5 border border-cranberry lg:mt-5 mt-2 hover:bg-cranberry hover:text-white transition-all"><IoMail size={20} />sandinu.rotaract3220@gmail.com</a>
                 </div>
             </div>
        </div>
