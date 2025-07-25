82f5260b14a0d357f5bfffa87df4394dfe9001b8
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-07-25 19:53:23 +0530

    HCV Update

--- disco-web/src/app/events/components/Event.jsx
+++ disco-web/src/app/events/components/Event.jsx
@@ -6,19 +6,20 @@ import Countdown from './Countdown'
 const Event = () => {
   return (
     <div className="grid lg:grid-cols-3 items-center justify-center w-full bg-gradient-to-r from-cranberry via-cranberry/80 text-white to-cranberry  animate-gradient rounded-xl lg:py-10 py-5 lg:px-10 px-5 lg:gap-10 gap-4 relative">
-      <Image src="/dcm25.jpg" width={400} height={500} className="rounded-lg lg:scale-100 scale-90" alt=''/>
+      <Image src="/HCV.png" width={400} height={500} className="rounded-lg lg:scale-100 scale-90" alt=''/>
       <div className='lg:col-span-2'>
         <div className='lg:absolute top-4 right-4 scale-90 lg:scale-100'>
-            <Countdown targetDateTime="2025-07-12T10:00:00"/>
+            <Countdown targetDateTime="2025-08-09T05:00:00"/>
         </div>
 
-        <h1 className="font-bebas lg:text-6xl text-6xl lg:text-left text-center tracking-wide mt-8">DISTRICT COUNCIL MEETING - JULY</h1>
+        <h1 className="font-bebas lg:text-6xl text-6xl lg:text-left text-center tracking-wide mt-8">Hill Country Visit</h1>
         <div className="flex lg:flex-row flex-col gap-3 mt-3 px-5 lg:px-0">
-            <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-1/3 w-full justify-center"><Calendar/> 12th of July 2025</h2>
-            <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-1/3 w-full justify-center"><Clock/> 10.00am Onwards</h2>
+            <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-1/3 w-full justify-center"><Calendar/> 9th & 10th of August 2025</h2>
+            <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-2/5 w-full justify-center"><LocateIcon/>Badulla, Sri Lanka</h2>
+            {/* <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-1/3 w-full justify-center"><Clock/> 10.00am Onwards</h2> */}
         </div>
         <div className="flex lg:flex-row flex-col gap-3 mt-3 px-5 lg:px-0">
-            <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-2/5 w-full justify-center"><LocateIcon/> Faculty of Arts, University of Colombo</h2>
+            {/* <h2 className="flex gap-2 bg-white/20 border border-white px-4 py-2 rounded-full lg:w-2/5 w-full justify-center"><LocateIcon/> Faculty of Arts, University of Colombo</h2> */}
         </div>
             
 
